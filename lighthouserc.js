92c8fc696291b8177de7aff0f35d47819491cac2
junzzi <jjh01027717502@gmail.com>
2022-08-18 16:58:46 +0900

    lighthouse 설정

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
       staticDistDir: "./build",
       // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
       url: ["http://localhost:3000"],
-      numberOfRuns: 5,
+      numberOfRuns: 1,
     },
     upload: {
       target: "filesystem",
